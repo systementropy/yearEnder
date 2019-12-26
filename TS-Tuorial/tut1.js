80b5e3ed18f218877e0b73d796dcc1046755a074
systementropy <vatsa.saket@gmail.com>
2019-12-26 12:13:09 +0530

    yeAR ender

---
+++ TS-Tuorial/tut1.js
@@ -0,0 +1,5 @@
+var myName = "derek";
+var myAge = 421;
+var canVote = true;
+var anyThing = "derek";
+document.getElementById("tsStuff").innerHTML = "my name is " + myName;
---
+++ TS-Tuorial/tut1.ts
@@ -0,0 +1,6 @@
+var myName: string = "derek";
+var myAge: number = 421;
+var canVote: boolean = true;
+var anyThing: any = "derek";
+
+document.getElementById("tsStuff").innerHTML = "my name is "+myName;
