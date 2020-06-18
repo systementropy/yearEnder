add411e3db5a2f130bd6a8fa372c2a0603844bf9
systementropy <vatsa.saket@gmail.com>
2020-06-18 22:29:18 +0530

    0618: Worst hit countries

--- test.js
+++ test.js
@@ -1,4 +1,22 @@
-let thisdata = {data: "[{'username': '9667512732', 'usertype': 'patient', 'download_speed': '2.70 Mbps', 'upload_speed': '2.70 Mbps', 'connected_on': 'Unknown'}]"}
-thisdata = thisdata.data.replace(/ /g,'').replace(/'/g,'"')
-thisdata = thisdata.substring(1,thisdata.length-1);
-console.log(JSON.parse(thisdata).username)
+let hr = 00;
+function activeHrs(time){
+	let srtTime,endTime;
+	time==0?
+		srtTime='11PM'
+		:time-1>12?srtTime=time-1-12+'PM':srtTime=time-1+'AM';
+	
+	time==23 || time==24?
+		endTime = time+2-24+'AM'
+		:time+2>12?endTime=time+2-12+'PM':endTime=time+2+'AM';
+	return (srtTime+'-'+endTime)
+}
+function getAMPM(time){
+	let hrs = time[0]
+	let mins = time[1]
+}
+let now = new Date();
+console.log(now.toLocaleTimeString())
+let stTime = new Date(now.setHours(now.getHours()-1)).toLocaleTimeString();
+let enTime = new Date(now.setHours(now.getHours()+3)).toLocaleTimeString();
+console.log(stTime,enTime)
+console.log(getAMPM([12,40]))
