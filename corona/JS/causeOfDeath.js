a0d3911fa2a6f3aae5aa2c26ddc7d063cc3c399f
systementropy <vatsa.saket@gmail.com>
2020-07-27 20:33:17 +0530

    cause of deaths

--- corona/JS/causeOfDeath.js
+++ corona/JS/causeOfDeath.js
@@ -1,69 +1,32 @@
 $(document).ready(function(){
-	
-	const legends = [
-		
-	];
-	const csvData = $.ajax({'url': "/data/datasets_557629_1366084_covid_19_india.csv", 'async': false}).responseText;
-	let rowCsvData = csvData.split(/\r?\n|\r/);
-	const dateWiseData = {}
-	
-	
-	for (let index = 1; index < rowCsvData.length; index++) {
-		const element = rowCsvData[index].split(',');
-		const prement = rowCsvData[index-1];
-		if(!element[1]){
-			// console.log(element)
-		}else{
-			let thisDate = element[1].split('/');
-			let thisState = element[3]
-			// console.log(thisDate)
-			if(index>1 && thisDate[0] === legends[legends.length-1][0]){
-				
-			}else if(index == 1){
-				legends.push(thisDate)
-			}else if(index>1 && thisDate[0] !== legends[legends.length-1][0]){
-				legends.push(thisDate)
-			}
-			if(!dateWiseData[thisState]){
-				let emptyArray = new Array(Math.max(0,legends.length-1))
-				emptyArray.fill(0,Math.max(0,legends.length-1))
-				// console.log(legends.length,emptyArray,thisState)
-				// dateWiseData[thisState] = [].fill(0,legends.length-1)
-				dateWiseData[thisState] = emptyArray;
-				dateWiseData[thisState].push(parseInt(element[8]))
-			}else if(index == 1){
-				dateWiseData[thisState] = [];
-				dateWiseData[thisState].push(parseInt(element[8]))
-			}else{
-				dateWiseData[thisState].push(parseInt(element[8]))
-			}
-			if(index === 1){
-				// console.log(element[1],thisDate)
-				// console.log(element)
-			}	
-		}
-		
-		
+	const dateWiseData = {
 	}
-	console.log(legends)
-	console.log(dateWiseData)
-	// for (const state in dateWiseData) {
-	// 	if (dateWiseData.hasOwnProperty(state)) {
-	// 		const element = dateWiseData[state];
-	// 		console.log(state,element.length)
-	// 	}
-	// }
-	var maxArr = 10;
+	const legends = ['March','March','May','July'];
+	const rawData = [[['Diabetes',0],['TB',0],['Suicide',0],['HIV/AIDS',0],['Malaria',0],['Malnutrition',0],['Homicide',0],['Parkinsons',0],['Meningitis',0],['Influenza',0],['Maternal',0],['Alcohol',0],['Drugs',0],['Conflict',0],['Gastro*',0],['Hepatitis',0],['Fire',0],['Poisonings',0],['COVID',0],['Heat/cold',0]],
+	[['Diabetes',315613],['TB',271883],['Suicide',188880],['HIV/AIDS',159026],['Malaria',148934],['Malnutrition',121687],['Homicide',93936],['Parkinsons',79135],['Meningitis',70389],['Influenza',47598],['Maternal',46673],['Alcohol',43225],['Drugs',38012],['Conflict',37675],['Gastro*',37002],['Hepatitis',30191],['Fire',28845],['Poisonings',17492],['COVID',19293],['Heat/cold',12867]],
+	[['Diabetes',546834],['TB',471067],['Suicide',327255],['HIV/AIDS',275530],['Malaria',258045],['Malnutrition',210836],['Homicide',162753],['Parkinsons',137109],['Meningitis',121956],['Influenza',82470],['Maternal',80867],['Alcohol',74893],['Drugs',65859],['Conflict',65276],['Gastro*',64111],['Hepatitis',52308],['Fire',49977],['Poisonings',30307],['COVID',346239],['Heat/cold',22293]],
+	[['Diabetes',773118],['TB',665998],['COVID',635666],['Suicide',462676],['HIV/AIDS',389546],['Malaria',364826],['Malnutrition',298082],['Homicide',249242],['Parkinsons',193846],['Meningitis',172422],['Influenza',116596],['Maternal',114330],['Alcohol',105884],['Drugs',93112],['Conflict',92288],['Gastro*',90640],['Hepatitis',73954],['Fire',40676],['Poisonings',42848],['Heat/cold',31518]]]
+
+	for (let index = 0; index < rawData.length; index++) {
+		const element = rawData[index];
+		for (let dtaPts = 0; dtaPts < element.length; dtaPts++) {
+			const each = element[dtaPts];
+			if(dateWiseData[each[0]]){
+				dateWiseData[each[0]].push(each[1])
+			}else{
+				dateWiseData[each[0]]=[]
+				dateWiseData[each[0]].push(each[1])
+			}
+		}
+	}
+	var maxArr = 20;
 	var barArray = [];
 	let count = 0; 
-	// const ticks = legends.length;
 	const ticks = legends.length;
-	
 	let tick;
-	// const areaIndex = {"Asia Pacific":1,"Europe":4,}
+	
 	var canvas = document.getElementById("canvas");
-	// var points = [];
-	var canHgt = 450;
+	var canHgt = 600;
 	var canWid = 780;
 	
 	canvas.height = canHgt*2;
@@ -77,13 +40,13 @@ $(document).ready(function(){
 	ctx.strokeStyle = '#FFFDDD';
 	ctx.fillStyle = 'rgba(255,0,0,1)';
 	ctx.scale(2,2);
-	const secs  = 20;
-	let globalCounter = (62*secs)-1;
-	const lengthFactor = 640;
+	const secs  = 100;
+	let globalCounter = 0;
+	const lengthFactor = 2000;
 	var colorArray =['#000000','#EE999F','#F0DDB8','#F0C38F','#D0DEc2','#AECDAD','#999FEE','#AECDAD'];
 	function Bar(x, y, length, dl, color, index, label, countryCode){
 		this.posX = x;
-		this.width = 25;
+		this.width = 20;
 		this.posY = 1+ (y * (this.width+1));
 		this.dl = dl;
 		this.length = 20;
@@ -96,6 +59,7 @@ $(document).ready(function(){
 		this.previousIndex;
 		this.dy = 0;
 		this.dx = 0;
+		this.yOffset = 10;
 		this.countryCode='';
 		
 		this.update = function(i,dateCounter){
@@ -103,8 +67,8 @@ $(document).ready(function(){
 				
 				this.previousIndex = this.index;
 				this.index = i;
-				if(Math.abs(this.previousIndex - this.index)>0.0001){
-					this.dy = (this.index-this.previousIndex)/10;
+				if(this.index !== this.previousIndex){
+					this.dy = (this.index-this.previousIndex)/(secs*0.75);
 				}else{
 					this.previousIndex = this.index;
 					this.dy = 0;
@@ -112,10 +76,10 @@ $(document).ready(function(){
 
 				this.tick = dateCounter;
 				
-				this.dx = (this.dl[dateCounter]-this.dl[dateCounter-1])/10;
+				this.dx = (this.dl[dateCounter]-this.dl[dateCounter-1])/(secs*0.75);
 				
 				
-				this.posY = (((this.previousIndex+this.dy) * (this.width+21)));
+				this.posY = (((this.previousIndex+this.dy) * (this.width+this.yOffset)));
 				// this.posY = (((this.index) * (this.width+15)));
 				this.dateCounter =this.dl[dateCounter];
 				if(this.dx == 0){
@@ -141,8 +105,8 @@ $(document).ready(function(){
 					this.length = this.length + (this.dx/lengthFactor);
 				}
 				
-				if(Math.abs(this.previousIndex - this.index)>0.0001){
-					this.posY = (((this.previousIndex+this.dy) * (this.width+21)));
+				if(Math.abs(this.previousIndex - this.index)>0.001){
+					this.posY = (((this.previousIndex+this.dy) * (this.width+this.yOffset)));
 					this.previousIndex += this.dy;
 					if(this.posY<0){
 						console.table(this.label,this.posY)
@@ -161,7 +125,7 @@ $(document).ready(function(){
 				ctx.fillStyle = '#E6E5E5';
 				ctx.strokeStyle = this.color;
 				ctx.lineWidth = 1;
-				ctx.rect(this.posX+20, this.posY+8, canWid, 8);
+				ctx.rect(this.posX+20, this.posY, canWid, this.width);
 				ctx.fill();
 				ctx.closePath();
 
@@ -179,7 +143,7 @@ $(document).ready(function(){
 				ctx.fillStyle = '#5A5A5A';
 				ctx.textAlign = 'left';
 				ctx.font = '500 21px/48px Montserrat';
-				// this.label == 'India'?ctx.fillStyle=this.color:'';
+				// this.label == 'COVID'?ctx.fillStyle=this.color:'';
 				ctx.textAlign = 'right';
 				ctx.fillText(this.label, this.posX -25 , this.posY+8+(this.width/2));
 				this.textLabelWidth = ctx.measureText(this.label).width;
@@ -188,8 +152,8 @@ $(document).ready(function(){
 				ctx.beginPath();
 				ctx.fillStyle = '#5A5A5A';
 				ctx.textAlign = 'left';
-				this.label == 'India'?ctx.fillStyle='#2B2A2A':'';
-				this.label == 'India'?ctx.font = '900 21px/48px Montserrat':ctx.font = '500 21px/48px Montserrat';
+				this.label == 'COVID'?ctx.fillStyle='#2B2A2A':'';
+				this.label == 'COVID'?ctx.font = '900 21px/48px Montserrat':ctx.font = '500 21px/48px Montserrat';
 				
 				// ctx.textAlign = "left"
 				ctx.fillText(this.dateCounterLabel, this.length+ this.posX + 30 , this.posY+8+(this.width/2));
@@ -219,14 +183,23 @@ $(document).ready(function(){
 			for (var i = 0; i < barArray.length; i++) {
 				barArray[i].update(i,tick);
 			}
-			if(legends[tick] && legends[tick][0]){
-				let month;
-				let date;
-				
-				const monthNames = ["--","January","February","March","April","May","June","July","August","September","October","November","December"];
-				date = legends[tick][0]
-				month = monthNames[parseInt(legends[tick][1])]
-				$('.legend1').text(`${month} ${date}, 2020`)
+			switch (tick) {
+				case 0:
+					$('.legend1>span').eq(0).addClass('active')
+					break;
+				case 1:
+					$('.legend1>span').eq(0).addClass('active')
+					break;
+				case 2:
+					$('.legend1>span').eq(0).removeClass('active')
+					$('.legend1>span').eq(2).addClass('active')
+					break;
+				case 3:
+					$('.legend1>span').eq(2).removeClass('active')
+					$('.legend1>span').eq(4).addClass('active')
+					break;
+				default:
+					break;
 			}
 		}else{
 		}
@@ -267,8 +240,8 @@ $(document).ready(function(){
 			if (dataTotal.hasOwnProperty(country) && country!=='Unassigned') {
 				const element = dataTotal[country];
 				let color;
-				country == 'India'?color = '#009DFF':color = '#FF6E6E'
-				const contArr = [ 'INDIA','China','US','Russia','South Korea','Iran','Turkey','UK','Germany','Italy','Spain'];
+				country == 'COVID'?color = '#009DFF':color = '#FF6E6E'
+				const contArr = [ 'COVID','China','US','Russia','South Korea','Iran','Turkey','UK','Germany','Italy','Spain'];
 					barArray.push(new Bar(
 						220,
 						count,
@@ -285,7 +258,7 @@ $(document).ready(function(){
 		}
 		setTimeout(()=>{
 			allTimeAnimate();
-			animateCircles(62);
+			animateCircles(0);
 		},2000)
 	};
 	init();
